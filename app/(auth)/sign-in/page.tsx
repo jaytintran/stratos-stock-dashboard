ded8a246d82f5157e90a74412c8bbedff53f5b47
Tin <jaytintran@gmail.com>
2025-10-16 01:39:11 +0700

    implement auth ui and layout

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,5 +1,79 @@
+"use client";
+
+import FooterLink from "@/components/forms/FooterLink";
+import InputField from "@/components/forms/InputField";
+import { Button } from "@/components/ui/button";
+import { useForm, SubmitHandler } from "react-hook-form";
+
 const SignIn = () => {
-	return <div>SignIn</div>;
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isSubmitting },
+	} = useForm<SignInFormData>({
+		mode: "onBlur",
+		defaultValues: {
+			email: "",
+			password: "",
+		},
+	});
+	const onSubmit = async (data: SignInFormData) => {
+		try {
+			console.log(data);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	return (
+		<div>
+			<h1 className="text-2xl font-bold text-gray-200 not-italic mb-5">
+				Welcome Back
+			</h1>
+
+			<form onSubmit={handleSubmit(onSubmit)} className="my-10">
+				<InputField
+					name="email"
+					label="Email"
+					placeholder="johndoe@gmail.com"
+					register={register}
+					error={errors.email}
+					validation={{
+						required: "Email is required",
+						pattern: {
+							value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+							message: "Invalid email address",
+						},
+					}}
+				/>
+
+				<InputField
+					name="password"
+					label="Password"
+					placeholder="Password"
+					type="password"
+					register={register}
+					error={errors.password}
+					validation={{ required: "Password is required", minLength: 6 }}
+				/>
+
+				<Button
+					type="submit"
+					disabled={isSubmitting}
+					className="yellow-btn w-full mt-5 !py-2"
+					size="lg"
+				>
+					{isSubmitting ? "Signing in..." : "Sign In"}
+				</Button>
+
+				<FooterLink
+					text="Don't have an account?"
+					linkText="Sign Up"
+					href="/sign-up"
+				/>
+			</form>
+		</div>
+	);
 };
 
 export default SignIn;
